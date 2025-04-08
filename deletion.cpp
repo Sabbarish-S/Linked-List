4a229b70d1136f6fd63b975f6ff78b30e675c3d5
Sabbarish-S <sabbarish.ss@gmail.com>
2025-04-08 14:45:24 +0530

    Deletion - Singly Linked List

---
+++ deletion.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> arr)
+{
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size(); i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void print(Node* head)
+{
+    while(head != NULL)
+    {
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+Node* removehead(Node*head)
+{
+    if(head == NULL) return head;
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+
+}
+
+int main()
+{
+    vector<int> arr = {1,2,3,4,5};
+    for(int i = 0; i < arr.size(); i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    Node* head = convertArr2LL(arr);
+    head=removehead(head);
+    cout<<"After the removal of head : "<<endl;
+    print(head);
+}
