e9658bc45161a3c040380a9899e505f64db7f282
Sabbarish-S <sabbarish.ss@gmail.com>
2025-04-11 21:58:39 +0530

    Deletion Doubly Linked List

--- deletiond.cpp
+++ deletiond.cpp
@@ -16,5 +16,41 @@ struct Node
             prev = prev1;
         }
     public:
-        Node(int data)
+        Node(int data1)
+        {
+            data = data1;
+            next = nullptr;
+            prev = nullptr;
+        }
 };
+
+Node* convertArr2DLL(vector<int> arr)
+{
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i< arr.size(); i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover=temp; 
+    }
+    return head;
+}
+
+void print(Node* head, vector<int> arr)
+{
+    for(int i = 0; i < arr.size(); i++)
+    {
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {23,43,12,74,12};
+    Node* head = convertArr2DLL(arr);
+    print(head,arr);
+
+}
+
