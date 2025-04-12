46af6a544235e938d1d68217173cb8b5fec85335
Sabbarish-S <sabbarish.ss@gmail.com>
2025-04-12 17:39:57 +0530

    Deletion in Doubly Linked List- HEAD

--- deletiond.cpp
+++ deletiond.cpp
@@ -6,21 +6,21 @@ struct Node
     public:
         int data;
         Node* next;
-        Node* prev;
+        Node* back;
 
     public:
-        Node(int data1, Node* next1, Node* prev1)
+        Node(int data1, Node* next1, Node* back1)
         {
             data = data1;
             next = next1;
-            prev = prev1;
+            back =back1;
         }
     public:
         Node(int data1)
         {
             data = data1;
             next = nullptr;
-            prev = nullptr;
+            back = nullptr;
         }
 };
 
@@ -46,10 +46,23 @@ void print(Node* head)
     }
 }
 
+Node* deleteHead(Node* head)
+{
+    if(head == NULL || head->next == NULL){ return NULL;}
+    Node* temp = head;
+    head = head->next;
+    head->back = nullptr;
+    temp->next = nullptr;
+
+    delete temp;
+    return head;
+}
+
 int main()
 {
     vector<int> arr = {23,43,12,74,12};
     Node* head = convertArr2DLL(arr);
+    head = deleteHead(head);
     print(head);
 
 }
